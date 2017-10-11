4a5a509c3b9f108ba335b7ec9036af5ec7998b13
Luke Dotson <lukedotson@gmail.com>
2017-10-10 22:08:43 -0400

    removing last name

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res){
-  res.send('Hello world from Luke Dotson!');
+  res.send('Hello world from Luke!');
 });
 
 app.listen(PORT);
