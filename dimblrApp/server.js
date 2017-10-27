66398ef684ac22ead36e9ce577915e636d407b33
Luke Dotson <lukedotson@gmail.com>
2017-10-26 20:45:55 -0400

    mongo test

--- dimblrApp/server.js
+++ dimblrApp/server.js
@@ -1,4 +1,15 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var dbURL = 'mongodb://localhost:27017/luke';
+
+var response;
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  response = "Connected correctly to server.";
+  db.clost();
+}
 
 // Constants
 var PORT = 8080;
@@ -7,7 +18,7 @@ var HOST = '0.0.0.0';
 // App
 var app = express();
 app.get('/', function (req, res){
-  res.send('Winning!');
+  res.send(response);
 });
 
 app.listen(PORT, HOST);
