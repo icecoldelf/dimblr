6e59983e53b648125859e80b5efe3d6ddd84863c
Luke Dotson <lukedotson@gmail.com>
2017-10-30 16:25:43 -0400

    asdf

--- dimblrApp/server.js
+++ dimblrApp/server.js
@@ -1,66 +1,31 @@
-var express = require('express'),
-    MongoClient = require('mongodb').MongoClient,
-    Db = require('mongodb').Db,
-    ObjectID = require('mongodb').ObjectID,
-    Server = require('mongodb').Server,
-    assert = require('assert');
+const express = require('express'),
+      MongoClient = require('mongodb').MongoClient,
+      Db = require('mongodb').Db,
+      ObjectID = require('mongodb').ObjectID,
+      Server = require('mongodb').Server,
+      assert = require('assert');
 
-    //dbURL = 'mongodb://172.17.0.3:27017/luke';
+
 
 var response = "happy";
 var test;
-// Constants
 var PORT = 8080;
 var HOST = '0.0.0.0';
 
+var app = express();
+
 var getHome = function(callback) {
-
   var db = new Db('luke', new Server('172.17.0.3', 27017));
-  var length;
-  var err;
-
   var useDatabase = function(err, database) {
     database.collections(function(err, collections) {
       if(!err) {
-        var objectToInsert = new Object();
-        objectToInsert.name = "something";
-        objectToInsert.children = new Array();
-        objectToInsert.children.push("red","green","orange");
-        collections[0].insert(objectToInsert);
         var result = collections[0].collectionName;
         callback(err, result);
       }
     });
   }
-
   db.open(useDatabase);
-  //db.close();
 }
-
-/*
-var findTests = function(db, callback) {
-  cursor = db.collection('test').find();
-  cursor.each(function(err, doc) {
-    assert.equal(err, null);
-    if (doc != null) {
-      test = doc;
-    } else {
-      callback();
-    }
-  });
-}
-
-MongoClient.connect(dbURL, function(err, db) {
-  assert.equal(null, err);
-  test = db.collection('test').find();
-  response = test;
-  db.close();
-});
-*/
-
-// App
-var app = express();
-
 
 app.get('/', function (req, res){
   getHome(function (err, data){
@@ -73,4 +38,3 @@ app.get('/buck', function (req, res){
 });
 
 app.listen(PORT, HOST);
-console.log('Running on http://localhost:' + PORT);
