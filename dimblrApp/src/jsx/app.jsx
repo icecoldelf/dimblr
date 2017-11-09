98fcd01048cf2ba43131a1e2942bdfe195393535
Luke Dotson <lukedotson@gmail.com>
2017-11-09 18:27:26 -0500

    table

--- dimblrApp/src/jsx/app.jsx
+++ dimblrApp/src/jsx/app.jsx
@@ -10,8 +10,32 @@ class IssueFilter extends React.Component {
 
 class IssueTable extends React.Component {
     render() {
+        const borderStyle = {border: "1px solid silver", padding: 6};
         return (
-            <div>Issue Table</div>
+            <table style={{borderCollapse: "collapse"}}>
+                <thead>
+                    <tr>
+                        <th style={borderStyle}>ID</th>
+                        <th style={borderStyle}>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <IssueRow issue_id={1} issue_title="Buck wants food" />
+                    <IssueRow issue_id={2} issue_title="Luke won't give Buck food" />
+                </tbody>
+            </table>
+        )
+    }
+}
+
+class IssueRow extends React.Component {
+    render() {
+        const borderStyle = {border: "1px solid silver", padding: 4};
+        return (
+            <tr>
+                <td style={borderStyle}>{this.props.issue_id}</td>
+                <td style={borderStyle}>{this.props.issue_title}</td>
+            </tr>
         )
     }
 }
