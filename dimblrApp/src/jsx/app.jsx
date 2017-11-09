cf5af723fd19b7df64534cb61989523eb594b43f
Luke Dotson <lukedotson@gmail.com>
2017-11-09 15:03:50 -0500

    asdf

--- dimblrApp/src/jsx/app.jsx
+++ dimblrApp/src/jsx/app.jsx
@@ -1,10 +1,41 @@
 const contentNode = document.getElementById('contents');
 
+class IssueFilter extends React.Component {
+    render() {
+        return (
+            <div>Issue Filter</div>
+        )
+    }
+}
+
+class IssueTable extends React.Component {
+    render() {
+        return (
+            <div>Issue Table</div>
+        )
+    }
+}
+
+class IssueAdd extends React.Component {
+    render() {
+        return (
+            <div>Issue Add</div>
+        )
+    }
+}
+
 class IssueList extends React.Component {
     render() {
         return (
-            <div>This is a placeholder</div>
-        );
+            <div>
+                <h1>Issue Tracker</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable />
+                <hr />
+                <IssueAdd />
+            </div>
+        )
     }
 }
                     
