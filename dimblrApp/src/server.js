c85b9f80b957f2de329a4d6ce1d6ad6dcfeeb6fb
Luke Dotson <lukedotson@gmail.com>
2017-11-13 03:07:01 -0500

    'asdf

--- dimblrApp/src/server.js
+++ dimblrApp/src/server.js
@@ -13,24 +13,24 @@ var HOST = '0.0.0.0';
 var app = express();
 
 var getHome = function(callback) {
-  var db = new Db('luke', new Server('172.17.0.3', 27017));
-  var useDatabase = function(err, database) {
-    database.collections(function(err, collections) {
-      if(!err) {
-        var result = collections[0].collectionName;
-        callback(err, result);
-      }
-    });
-  }
-  db.open(useDatabase);
+  //var db = new Db('luke', new Server('172.17.0.3', 27017));
+  //var useDatabase = function(err, database) {
+    //database.collections(function(err, collections) {
+     // if(!err) {
+      //  var result = collections[0].collectionName;
+      //  callback(err, result);
+     // }
+    //});
+  //}
+  //db.open(useDatabase);
 }
 
 app.use("/static", express.static(__dirname + "/static"));
 
 app.get('/', function (req, res){
-  getHome(function (err, data){
-    res.send("<html><head><style>table.bordered-table th, td {border: 1px solid silver; padding: 4px;}table.bordered-table {border-collapse: collapse};</style><script src='https://cdnjs.cloudflare.com/ajax/libs/react/15.2.1/react.js'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/react/15.2.1/react-dom.js'></script></head><body><div id='contents'></div><script src='http://u.won.gg/static/app.js'></script></body></html>" + data);
-  });
+  //getHome(function (err, data){
+    res.send("<html><head><style>table.bordered-table th, td {border: 1px solid silver; padding: 4px;}table.bordered-table {border-collapse: collapse};</style><script src='https://cdnjs.cloudflare.com/ajax/libs/react/15.2.1/react.js'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/react/15.2.1/react-dom.js'></script></head><body><div id='contents'></div><script src='http://u.won.gg/static/app.js'></script></body></html>");
+  //});
 });
 
 app.get('/buck', function (req, res){
