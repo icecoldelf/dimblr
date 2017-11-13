9973841a0c10412deec7d64c7eacaa639df44e83
Luke Dotson <lukedotson@gmail.com>
2017-11-13 03:48:40 -0500

    asdf

--- dimblrApp/src/jsx/app.jsx
+++ dimblrApp/src/jsx/app.jsx
@@ -73,9 +73,30 @@ class IssueRow extends React.Component {
 }
 
 class IssueAdd extends React.Component {
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        var form = document.forms.issueAdd;
+        this.props.createIssue({
+            owner: form.owner.value,
+            title: form.title.value,
+            status: 'New',
+            created: new Date()
+        });
+        form.owner.value = ""; form.title.value = "";
+    }
     render() {
         return (
-            <div>Issue Add</div>
+            <div>
+                <form name="issueAdd" onSubmit={this.handleSubmit}>
+                    <input type="text" name="owner" placeholder="Owner" />
+                    <input type="text" name="title" placeholder="Title" />
+                    <button>Add</button>
+                </form>
+            </div>
         )
     }
 }
@@ -84,7 +105,7 @@ class IssueList extends React.Component {
     constructor() {
         super();
         this.state = { issues: [] };
-        this. createTestIssue = this.createTestIssue.bind(this);
+        this.createTestIssue = this.createTestIssue.bind(this);
         setTimeout(this.createTestIssue, 2000);
     }
     componentDidMount() {
