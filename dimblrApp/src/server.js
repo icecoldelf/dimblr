75ab04714329eb2e1568e31445c6c8aabe56637d
Luke Dotson <lukedotson@gmail.com>
2017-11-14 15:41:24 -0500

    asdf

--- dimblrApp/src/server.js
+++ dimblrApp/src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-app.use(express.static('static'));
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.listen(8080, function () {
   console.log('App started on port 8080');
