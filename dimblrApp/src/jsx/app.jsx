6d18ec1cc64aed7a19ac2db76914dde15ae50e5f
Luke Dotson <lukedotson@gmail.com>
2017-11-14 21:17:03 -0500

    asdf

--- dimblrApp/src/jsx/app.jsx
+++ dimblrApp/src/jsx/app.jsx
@@ -76,6 +76,7 @@ class IssueList extends React.Component {
         super();
         this.state = { issues: [] };
         this.createIssue = this.createIssue.bind(this);
+        setInterval(1000, this.loadData());
     }
     componentDidMount() {
         this.loadData();
